a153bca8e44687e28b0dbd890023f8dcfbde4a4d
erikchiodo <erikchiodo@gmail.com>
2023-04-11 22:20:28 -0400

    Added functionality to create SVG file (with default text)

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const userPrompts = [
   },
   {
     type: "input",
-    name: "text_color",
+    name: "text-color",
     message: "What text color would you like? (enter keyword or hexadecimal): ",
   },
 
@@ -22,15 +22,32 @@ const userPrompts = [
   },
   {
     type: "input",
-    name: "shape_color",
+    name: "shape-color",
     message:
       "What shape color would you like? (enter keyword or hexadecimal): ",
   },
 ];
 
+function createFile(fileName, data) {
+  try {
+    let filePath = path.join(__dirname, "/distri/", fileName);
+    fs.writeFileSync(filePath, data);
+    console.log("Generated logo.svg");
+  } catch (error) {
+    console.log("Error generating file");
+    console.error(error);
+  }
+}
+
 function init() {
   inquirer.prompt(userPrompts).then((userResponses) => {
     console.log(userResponses);
+    const svgFile = `<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="50" cy="50" r="45" fill="#FFA500" />
+  <text x="50" y="55" fill="#ffffff" text-anchor="middle">Sample Text</text>
+</svg>
+`;
+    createFile("svg.logo", svgFile);
   });
 }
 
