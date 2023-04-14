d5481c6e0999db645817a575c8ddbf88325d9833
erikchiodo <erikchiodo@gmail.com>
2023-04-14 12:03:54 -0400

    Finalized code changes

--- index.js
+++ index.js
@@ -29,11 +29,12 @@ const userPrompts = [
   },
 ];
 
+// Function to Create Logo.svg once user answers prompts
 function createFile(fileName, data) {
   try {
-    let filePath = path.join(__dirname, "/distri/", fileName);
+    let filePath = path.join(__dirname, "/distri", fileName);
     fs.writeFileSync(filePath, data);
-    console.log("Generated logo.svg");
+    console.log("Generated logo.svg. Check distri folder.");
   } catch (error) {
     console.log("Error generating file");
     console.error(error);
@@ -42,9 +43,18 @@ function createFile(fileName, data) {
 
 async function init() {
   const response = await inquirer.prompt(userPrompts);
-  console.log(response);
 
-  // Create Logic to create SVG based on shape selected
+  // Conditional Statements to check if valid text input
+  if (response.text.length > 0 && response.text.length < 4) {
+    console.log("Valid Text Input");
+  } else {
+    console.log(
+      "ERROR: Invalid Text Input. Please input up to a minimum 1 character (i.e. 'A') and maximum 3 characters (i.e. 'ABC')"
+    );
+    return;
+  }
+
+  // Logic to Create Circle File
   if (response.shape === "circle") {
     console.log("Circle Selected");
     let circleSVG = new CircleSVG(
@@ -54,9 +64,9 @@ async function init() {
       response.shapeColor
     );
     let renderedSVG = circleSVG.render();
-    console.log(renderedSVG);
-    fs.writeFileSync("./distri/logo.svg", renderedSVG);
-    console.log("Circle File Created");
+    createFile("logo.svg", renderedSVG);
+
+    // Logic to Create Triangle File
   } else if (response.shape === "triangle") {
     let triangleSVG = new TriangleSVG(
       response.text,
@@ -65,8 +75,9 @@ async function init() {
       response.shapeColor
     );
     let renderedSVG = triangleSVG.render();
-    fs.writeFileSync("./distri/logo.svg", renderedSVG);
-    console.log("Triangle File Created");
+    createFile("logo.svg", renderedSVG);
+
+    // Logic to Create Square File
   } else if (response.shape === "square") {
     console.log("Square Selected");
     let squareSVG = new SquareSVG(
@@ -76,9 +87,7 @@ async function init() {
       response.shapeColor
     );
     let renderedSVG = squareSVG.render();
-    console.log(renderedSVG);
-    fs.writeFileSync("./distri/logo.svg", renderedSVG);
-    console.log("Square File Created");
+    createFile("logo.svg", renderedSVG);
   } else {
     console.log(
       "Error: Please pick shape. Re-run node index.js and follow prompts again"
