2666d091b0b154db32430c4cd8fdb45d862a4fa2
erikchiodo <erikchiodo@gmail.com>
2023-04-14 12:04:11 -0400

    Finalized Code Changes

--- assets/shapes.js
+++ assets/shapes.js
@@ -15,7 +15,7 @@ class CircleSVG extends svgAttributes {
   render() {
     return `<svg viewBox="0 0 300 200" xmlns="http://www.w3.org/2000/svg">
     <circle cx="150" cy="100" r= "100" fill="${this.shapeColor}" />
-    <text x="150" y="115" font-size="60" fill="${this.textColor}" text-anchor="middle">${this.text}</text>
+    <text x="150" y="120" font-size="60" fill="${this.textColor}" text-anchor="middle">${this.text}</text>
     </svg>`;
   }
 }
